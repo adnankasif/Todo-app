bb73c78db7d4167d2c1abf7bd2771d2aa1a739a7
kasif <adnankasif@gmail.com>
2016-10-09 20:55:51 +0530

    optimizing

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -16,21 +16,6 @@ class DashboardController < ApplicationController
   def project_manager_dashboard
   	begin
       @line_chart  = GenerateChart.generate_pie_chart
-      # @line_chart = []
-      # @all_projects = Project.all
-      # @all_projects.each do |project|
-
-      #   all_todos = Todo.where("project_id = ?", project.id).pluck(:status)
-      #   new_c = all_todos.count("new")
-      #   inp_c = all_todos.count("in progress")
-      #   done_c = all_todos.count("done")
-      #   arr = [done_c,inp_c,new_c]
-      #   lab = ["Done(#{done_c})", "In progress(#{inp_c})", "New(#{new_c})" ]
-      #   project_name = project.name
-      #   if !(new_c == 0 && inp_c == 0 && done_c == 0)
-  		  #   @line_chart << Gchart.pie(:data => arr, :title => "Project:#{project_name}" , :size => '400x200', :labels => lab)
-      #   end
-      # end
   	rescue Exception => e
   		Rails.logger.error "message: #{e.message}"
   	end
